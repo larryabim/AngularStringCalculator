fbce87d6985334ec38b275f2dff20fc105b9b95b
Larry <larryabim@gmail.com>
2020-01-14 17:54:16 -0600

    Have a working Angular String calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'string-calculator';
+  testInput: string;
+  calculator: number;
+  regex = /-?[0-9]+/g;
+
+  constructor() {
+
+  }
+
+  numDigits(x: number): number {
+    return Math.max(Math.floor(Math.log10(Math.abs(x))), 0) + 1;
+  }
+
+  checkDigits(array: any ): any {
+    for (let i = 0; i < array.length; i++) {
+      if (this.numDigits(Number(array[i])) > 3 ) {
+        array[i] = 0;
+      }
+    }
+    return array;
+  }
+
+  stringCalculator(input: any): void {
+    if (!input) {
+      this.calculator = 0;
+      return;
+    }
+    const text = input.toString();
+    const stringArray =  this.checkDigits(text.match(this.regex));
+    this.calculator = stringArray.reduce((t, s) => {
+      return Number(t) + Number(s);
+    });
+  }
+
 }
+
